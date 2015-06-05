0da9cc0f0a6cfefd011925cfa32eba157d7f875d
Ignatyev, Konstantin <ignatyev@cobalt.com>
2015-06-05 16:27:05 -0700

    made maven buildable; migrated to jdom2;

--- src/test/java/net/divbyzero/gpx/tests/JDOMTest.java
+++ src/test/java/net/divbyzero/gpx/tests/JDOMTest.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.divbyzero.gpx.tests.parsers;
+package net.divbyzero.gpx.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
